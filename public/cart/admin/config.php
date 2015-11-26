0097ec3cc93df947b8d9738eb3684b4cae84b0c3
Robert Lengyel <robert.lengyel7@gmail.com>
2015-11-26 15:24:19 -0500

    Installed the cart thingy -> but looking through files it looks like it has dependencies to my system -> change this if possible

---
+++ public/cart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:800/cart/admin/');
+define('HTTP_CATALOG', 'http://localhost:800/cart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:800/cart/admin/');
+define('HTTPS_CATALOG', 'http://localhost:800/cart/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/admin/');
+define('DIR_SYSTEM', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/');
+define('DIR_LANGUAGE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/admin/language/');
+define('DIR_TEMPLATE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/admin/view/template/');
+define('DIR_CONFIG', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/config/');
+define('DIR_IMAGE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/image/');
+define('DIR_CACHE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/download/');
+define('DIR_LOGS', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/upload/');
+define('DIR_CATALOG', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-03.cleardb.net');
+define('DB_USERNAME', 'b8e0534712ff58');
+define('DB_PASSWORD', '09fd9d84');
+define('DB_DATABASE', 'heroku_ac235d9b419712c');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ public/cart/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:800/cart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:800/cart/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/catalog/');
+define('DIR_SYSTEM', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/');
+define('DIR_LANGUAGE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/catalog/language/');
+define('DIR_TEMPLATE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/config/');
+define('DIR_IMAGE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/image/');
+define('DIR_CACHE', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/download/');
+define('DIR_LOGS', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/Users/JohnSmith/Desktop/Drone Lens/Install Open With Server/ROOT/cart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'us-cdbr-iron-east-03.cleardb.net');
+define('DB_USERNAME', 'b8e0534712ff58');
+define('DB_PASSWORD', '09fd9d84');
+define('DB_DATABASE', 'heroku_ac235d9b419712c');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
